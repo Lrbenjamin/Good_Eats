4575c09d3796a53ce267c53673eaf9c9efea24d8
Caleb Warford <cjwarford98@gmail.com>
2024-08-02 18:37:31 -0700

    Initial website layout. Added images, index.html code, necessary config files, and fixed .gitignore to get rid of excess files

---
+++ client/main.js
@@ -0,0 +1,2 @@
+import "./assets/css/tailwind.css";
+var rellax = new Rellax(".rellax");
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [react()]
+    server: {
+        port: process.env.PORT || 3000, // Use the PORT environment variable if available, or default to 3000
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+            },
+        },
+    },
 });
