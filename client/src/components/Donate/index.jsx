5e11c25917dd9a9dbe1f458f3a7d297d9768d74e
Caleb Warford <cjwarford98@gmail.com>
2024-08-03 18:08:36 -0700

    initial design for donate page and moved config files to config folder

--- client/src/components/Donate/index.jsx
+++ client/src/components/Donate/index.jsx
@@ -1,1 +1,74 @@
-<body>This will be the Donate page!!!!!</body>
+<body class="bg-black selection:bg-white/10">
+<main class="relative overflow-hidden">
+    <section id="introduction" class="relative top-0 z-10 bg-black pb-20 pt-20 md:sticky">
+        <div class="mx-auto max-w-7xl px-6 lg:px-12 xl:px-6 2xl:px-0">
+            <div class="flex">
+                <div class="w-full">
+                    <div data-rellax-speed="-2" data-rellax-xs-speed="0" data-rellax-mobile-speed="0" class="rellax flex flex-wrap items-center gap-6">
+                        <h2 class="text-7xl font-bold text-white xl:text-8xl">Why Donate</h2>
+                        <span class="h-max rounded-full border border-white/40 px-2 py-1 text-xs tracking-wider text-white">01</span>
+                    </div>
+                    <div class="mt-12 ml-auto text-lg font-light text-white sm:text-2xl md:w-2/3 lg:mt-0 lg:w-1/2 xl:leading-normal">
+                        <p>Donate to Good Eats to support local restaurants and foster vibrant communities! Your contribution helps us showcase and elevate unique dining experiences, driving more customers to local eateries and ensuring their success.</p>
+                        <p class="mt-12">By giving, you're not only helping businesses thrive, but also enriching your community with diverse and delicious food options.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="overview" class="relative z-10 bg-black pb-20 lg:pt-20">
+        <div class="xl:px-6">
+            <div class="grid grid-cols-2 gap-3 md:gap-6">
+                <div class="relative before:absolute before:inset-0 before:origin-top before:bg-gradient-to-t before:from-black/5 before:opacity-50 before:backdrop-grayscale before:transition before:duration-500 hover:before:origin-bottom hover:before:scale-y-0">
+                    <img class="transition duration-500" src="./public/images/projects/restaurant1.webp" alt="project image" width="1380" height="1380" />
+                </div>
+                <div data-rellax-speed="-2" data-rellax-xs-speed="0" data-rellax-mobile-speed="0" class="rellax relative before:absolute before:inset-0 before:origin-top before:bg-gradient-to-t before:from-black/5 before:opacity-50 before:backdrop-grayscale before:transition before:duration-500 hover:before:origin-bottom hover:before:scale-y-0">
+                    <img class="h-full w-full object-cover transition duration-500" src="./public/images/projects/restaurant2.webp" alt="project cover" width="2000" height="1333" />
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="approach" class="relative z-10 bg-black pb-20 pt-20 md:sticky md:top-12">
+        <div class="mx-auto max-w-7xl px-6 lg:px-12 xl:px-6 2xl:px-0">
+            <div class="flex">
+                <div class="w-full">
+                    <div class="flex flex-wrap items-center gap-6">
+                        <h2 class="text-7xl font-bold text-white xl:text-8xl">Our approach</h2>
+                        <span class="h-max rounded-full border border-white/40 px-2 py-1 text-xs tracking-wider text-white">02</span>
+                    </div>
+                    <div class="mt-24 ml-auto text-lg font-light text-white sm:text-2xl md:w-2/3 lg:w-1/2 xl:leading-normal">
+                        <p>At Good Eats, we’re committed to uplifting local businesses by dedicating 50% of our proceeds to their support. Our approach ensures that your contributions directly benefit the restaurants we promote, helping them thrive and enriching our community’s dining experience.</p>
+                        <p class="mt-12"> Join us in making a tangible impact where it matters most!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="overview" class="relative z-10 bg-black pb-20 lg:pt-20">
+        <div class="xl:px-6">
+            <div class="grid grid-cols-2 gap-3 md:gap-6">
+                <div class="relative before:absolute before:inset-0 before:origin-top before:bg-gradient-to-t before:from-black/5 before:opacity-50 before:backdrop-grayscale before:transition before:duration-500 hover:before:origin-bottom hover:before:scale-y-0">
+                    <img class="transition duration-500" src="./public/images/projects/restaurant3.jpg" alt="project image" width="1380" height="1380" />
+                </div>
+                <div data-rellax-speed="-2" data-rellax-xs-speed="0" data-rellax-mobile-speed="0" class="rellax relative before:absolute before:inset-0 before:origin-top before:bg-gradient-to-t before:from-black/5 before:opacity-50 before:backdrop-grayscale before:transition before:duration-500 hover:before:origin-bottom hover:before:scale-y-0">
+                    <img class="h-full w-full object-cover transition duration-500" src="./public/images/projects/restaurant4.jpg" alt="project cover" width="2000" height="1333" />
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="approach" class="relative z-10 bg-black py-20 md:sticky md:top-12">
+        <a href="/pages/project.html" class="block bg-gradient-to-b from-transparent to-white/10 py-32">
+            <div class="mx-auto max-w-7xl px-6 lg:px-12 xl:px-6 2xl:px-0">
+                <div class="flex">
+                    <div class="w-full">
+                        <span class="mx-auto block h-max w-max rounded-full border border-white/40 px-2 py-1 text-xs tracking-wider text-white">Click Here</span>
+                        <h2 class="text-center text-7xl font-bold text-white xl:text-8xl">Donate Now</h2>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </section>
+</main>
+<script type="module" src="./main.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/dixonandmoe/rellax@master/rellax.min.js"></script>
+</body>
