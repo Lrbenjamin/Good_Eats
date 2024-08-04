d944a9f6e0e9e335662f0159372d2bbdc9a815ab
Caleb Warford <cjwarford98@gmail.com>
2024-08-04 11:03:44 -0700

    Added new "our team" section. WIP

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -151,92 +151,83 @@ const Home = () => {
                 </a>
             </div>
         </section>
+        
         <footer className="padding-top: 4000px; relative bg-black pt-32 backdrop-opacity-0">
     <section id="about" className="relative z-10 bg-black pb-20 pt-32 md:pb-0 md:pt-0 lg:pb-0">
-        <section className="">
-            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
-                <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                    <h2 className="text-7xl font-bold text-white xl:text-8xl">About Us</h2>
-                </div>
-                <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
-                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="../../../../public/images/caleb.png" alt="Caleb Png"></img>
-                        </a>
-                        <div className="p-5">
-                            <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="#">Caleb Warford</a>
-                            </h3>
-                            <span className="text-gray-500 dark:text-gray-400">Front End Designer</span>
-                            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Likes anime or something idk man cool description though.</p>
-                            <ul className="flex space-x-4 sm:mt-0">
-                                <li>
-                                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+        <section>
+        <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+ 
+  <div class="grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
+    <div>
+      <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+        <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="../../../public/images/caleb.png" alt="Caleb.png" />
+        <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+          <p class="mb-1 text-lg font-bold text-gray-100">Caleb Warford</p>
+          <p class="mb-4 text-xs text-gray-100">Web Developer</p>
+          <p class="mb-4 text-xs tracking-wide text-gray-400">
+            Vincent Van Gogh’s most popular painting, The Starry Night.
+          </p>
+          <div class="flex items-center justify-center space-x-3">
+          <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
                                     </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="../../../../public/images/logan.jpg" alt="Logan Png"></img>
-                        </a>
-                        <div className="p-5">
-                            <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="#">Logan Benjamin</a>
-                            </h3>
-                            <span className="text-gray-500 dark:text-gray-400">Chuck Norris Enthusiast</span>
-                            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Was saved by chuck norris from a burning building as a child</p>
-                            <ul className="flex space-x-4 sm:mt-0">
-                                <li>
-                                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+        <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="../../../public/images/logan.jpg" alt="Logan.jpg" />
+        <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+          <p class="mb-1 text-lg font-bold text-gray-100">Logan Benjamin</p>
+          <p class="mb-4 text-xs text-gray-100">Web Developer</p>
+          <p class="mb-4 text-xs tracking-wide text-gray-400">
+            Chuck Norris saved me from a burning building as a child.
+          </p>
+          <div class="flex items-center justify-center space-x-3">
+          <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
                                     </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="Damien Png"></img>
-                        </a>
-                        <div className="p-5">
-                            <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="#">Damien Mendoza</a>
-                            </h3>
-                            <span className="text-gray-500 dark:text-gray-400">Senior Front-end Developer</span>
-                            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Man I really hope I don't spell someones name wrong</p>
-                            <ul className="flex space-x-4 sm:mt-0">
-                                <li>
-                                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+        <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="../../../public/images/annie.png" alt="Annie" />
+        <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+          <p class="mb-1 text-lg font-bold text-gray-100">Annie Schalnat</p>
+          <p class="mb-4 text-xs text-gray-100">Web Developer</p>
+          <p class="mb-4 text-xs tracking-wide text-gray-400">
+            Apple pie macaroon toffee jujubes pie tart cookie caramels.
+          </p>
+          <div class="flex items-center justify-center space-x-3">
+          <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
                                     </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="../../../../public/images/annie.png" alt="Annie Png"></img>
-                        </a>
-                        <div className="p-5">
-                            <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="#">Annie Schalnat</a>
-                            </h3>
-                            <span className="text-gray-500 dark:text-gray-400">Marketing & Sale</span>
-                            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Running out of things to type. Oh well man, it is what it is</p>
-                            <ul className="flex space-x-4 sm:mt-0">
-                                <li>
-                                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+        <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Damien.jpg" />
+        <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+          <p class="mb-1 text-lg font-bold text-gray-100">Damien Mendoza</p>
+          <p class="mb-4 text-xs text-gray-100">Web Developer</p>
+          <p class="mb-4 text-xs tracking-wide text-gray-400">
+            Lorizzle ipsum bling bling sit amizzle, consectetuer adipiscing elit.
+          </p>
+          <div class="flex items-center justify-center space-x-3">
+          <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
                                     </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
         </section>
     </section>
     <div className="mx-auto max-w-7xl px-6 pb-12 lg:px-12 xl:px-6 2xl:px-0">
