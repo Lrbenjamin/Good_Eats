04f853b7ced71da9eae26cedd05130691e51e477
Lrbenjamin <161282417+Lrbenjamin@users.noreply.github.com>
2024-08-12 21:02:20 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', 
+  base: './',
   plugins: [react()],
   server: {
     port: 3000,
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['apollo-server'],
+      external: [], 
     },
   },
 });
