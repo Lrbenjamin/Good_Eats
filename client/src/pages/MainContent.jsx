258c35e603aa4356fb5648345cc94750db9f72b5
Caleb Warford <cjwarford98@gmail.com>
2024-08-12 23:33:45 -0700

    ugh

--- client/src/pages/MainContent.jsx
+++ client/src/pages/MainContent.jsx
@@ -1,14 +1,14 @@
 // The main and only page in the website which imports components and establishes routes for the website
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home'; 
-import Profile from './Profile';
-import Signup from './Signup';
-import Login from './Login';
-import ErrorPage from './ErrorPage';
-import Donate from './Donate';
-import Store from './Store';
-import StoreEdit from './StoreEdit';
+import Home from '../components/Home'; 
+import Profile from '../components/Profile';
+import Signup from '../components/Signup';
+import Login from '../components/Login';
+import ErrorPage from '../components/ErrorPage';
+import Donate from '../components/Donate';
+import Store from '../components/Store';
+import StoreEdit from '../components/StoreEdit';
 
 const MainContent = () => {
   return (
