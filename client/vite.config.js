dcbac342a9ff6dfb5e6c06fdbabb5e715c5fb732
Logan Benjamin <Lrbenjamin22@gmail.com>
2024-08-12 23:48:30 -0700

    maybe?

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  plugins: [react()],
+  base: './', 
 });
