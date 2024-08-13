d7c75b570eb4039a657e896e4cb42d4b73e73972
Lrbenjamin <161282417+Lrbenjamin@users.noreply.github.com>
2024-08-12 19:54:00 -0700

    Update MainContent.jsx

--- client/src/pages/MainContent.jsx
+++ client/src/pages/MainContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from '../components/Home';
+import Home from '../components/home';
 import Profile from '../components/Profile';
 import Signup from '../components/Signup';
 import Login from '../components/Login';
