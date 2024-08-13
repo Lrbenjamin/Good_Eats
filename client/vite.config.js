c94af1218a2624f00cdedeb63955e906bb058638
Logan Benjamin <Lrbenjamin22@gmail.com>
2024-08-12 23:25:11 -0700

    i hate vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',  // Ensure relative paths for assets
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: './index.html',  // Ensure this points to the correct index.html
-      output: {
-        // Let Vite handle the file names automatically
-      },
-    },
-  },
-  root: './',  // Point to the current directory where the vite.config.js resides
+    assetsDir: 'assets'
+  }
 });
